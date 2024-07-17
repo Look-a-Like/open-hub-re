1cddb9b7d5c49267b6aa0f59028d53b932c6de02
SAISURYAtalla <tallasai@iitbhilai.ac.in>
2024-07-18 01:22:08 +0530

    settings page ui update

--- frontend/src/Setting_details/SettingContent.js
+++ frontend/src/Setting_details/SettingContent.js
@@ -100,10 +100,13 @@ const SettingsPage = () => {
     return (
         <div className="flex flex-col md:flex-row min-h-screen bg-pink-50">
             <div className="flex-1 flex flex-col items-center justify-center p-8">
-                <div className="w-full max-w-xl space-y-6">
+                <div className="w-full max-w-2xl space-y-6">
                     <div className="bg-white p-8 rounded-md shadow">
-                        <h2 className="text-xl font-bold mb-4">Change Password</h2>
-                        <div className="mb-4 space-y-4">
+                        <div className="flex items-center mb-4">
+                            <h2 className="text-xl font-bold mr-4">Change Password</h2>
+                            <div className="flex-1 border-t border-gray-300"></div>
+                        </div>
+                        <div className="flex flex-col md:flex-row mb-4 space-y-4 md:space-y-0 md:space-x-4">
                             <input
                                 type="password"
                                 name="currentPassword"
@@ -140,8 +143,11 @@ const SettingsPage = () => {
                     </div>
 
                     <div className="bg-white p-8 rounded-md shadow">
-                        <h2 className="text-xl font-bold mb-4">Change Email</h2>
-                        <div className="mb-4 space-y-4">
+                        <div className="flex items-center mb-4">
+                            <h2 className="text-xl font-bold mr-4">Change Email</h2>
+                            <div className="flex-1 border-t border-gray-300"></div>
+                        </div>
+                        <div className="flex flex-col md:flex-row mb-4 space-y-4 md:space-y-0 md:space-x-4">
                             <input
                                 type="email"
                                 name="email"
@@ -162,7 +168,10 @@ const SettingsPage = () => {
                     </div>
 
                     <div className="bg-white p-8 rounded-md shadow">
-                        <h2 className="text-xl font-bold mb-4">Session Management</h2>
+                        <div className="flex items-center mb-4">
+                            <h2 className="text-xl font-bold mr-4">Session Management</h2>
+                            <div className="flex-1 border-t border-gray-300"></div>
+                        </div>
                         <button
                             onClick={handleSignOut}
                             className="w-full p-2 bg-red-400 hover:bg-red-500 text-white rounded-md"
@@ -173,6 +182,81 @@ const SettingsPage = () => {
                 </div>
             </div>
         </div>
+        // <div className="flex flex-col md:flex-row min-h-screen bg-pink-50">
+        //     <div className="flex-1 flex flex-col items-center justify-center p-8">
+        //         <div className="w-full max-w-xl space-y-6">
+        //             <div className="bg-white p-6 rounded-md shadow">
+        //                 <h2 className="text-xl font-bold mb-4">Change Password</h2>
+        //                 <div className="mb-4 space-y-4">
+        //                     <input
+        //                         type="password"
+        //                         name="currentPassword"
+        //                         placeholder="Current Password"
+        //                         value={currentPassword}
+        //                         onChange={handlePasswordChange}
+        //                         className="w-full p-2 border rounded"
+        //                     />
+        //                     <input
+        //                         type="password"
+        //                         name="newPassword"
+        //                         placeholder="New Password"
+        //                         value={newPassword}
+        //                         onChange={handlePasswordChange}
+        //                         className="w-full p-2 border rounded"
+        //                     />
+        //                     <input
+        //                         type="password"
+        //                         name="confirmPassword"
+        //                         placeholder="Confirm Password"
+        //                         value={confirmPassword}
+        //                         onChange={handlePasswordChange}
+        //                         className="w-full p-2 border rounded"
+        //                     />
+        //                 </div>
+        //                 <button
+        //                     onClick={handlePasswordSubmit}
+        //                     disabled={!isPasswordActive}
+        //                     className={`w-full p-2 ${isPasswordActive ? 'bg-red-400 hover:bg-red-500' : 'bg-red-200'} text-white rounded-md`}
+        //                 >
+        //                     Change Password
+        //                 </button>
+        //                 {errorMessage && <p className="text-red-500 mt-2">{errorMessage}</p>}
+        //             </div>
+
+        //             <div className="bg-white p-8 rounded-md shadow">
+        //                 <h2 className="text-xl font-bold mb-4">Change Email</h2>
+        //                 <div className="mb-4 space-y-4">
+        //                     <input
+        //                         type="email"
+        //                         name="email"
+        //                         placeholder="Email Address"
+        //                         value={email}
+        //                         onChange={handleEmailChange}
+        //                         className="w-full p-2 border rounded"
+        //                     />
+        //                 </div>
+        //                 <button
+        //                     onClick={handleEmailSubmit}
+        //                     disabled={!isEmailActive}
+        //                     className={`w-full p-2 ${isEmailActive ? 'bg-red-400 hover:bg-red-500' : 'bg-red-200'} text-white rounded-md`}
+        //                 >
+        //                     Change Email
+        //                 </button>
+        //                 {errorMessage && <p className="text-red-500 mt-2">{errorMessage}</p>}
+        //             </div>
+
+        //             <div className="bg-white p-8 rounded-md shadow">
+        //                 <h2 className="text-xl font-bold mb-4">Session Management</h2>
+        //                 <button
+        //                     onClick={handleSignOut}
+        //                     className="w-full p-2 bg-red-400 hover:bg-red-500 text-white rounded-md"
+        //                 >
+        //                     Sign Out
+        //                 </button>
+        //             </div>
+        //         </div>
+        //     </div>
+        // </div>
     );
 };
 
